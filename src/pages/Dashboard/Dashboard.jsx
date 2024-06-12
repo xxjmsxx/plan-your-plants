3aa61fd716c47a34cae5c5b66f553b50709ade3a
Joshua Smith <sarjsmithde@gmail.com>
2024-06-12 12:08:44 +0200

    create Dashboard Page for after signin

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,10 +1,12 @@
-function Dashboard() {
+import LogoutButton from "../../components/LogoutButton/LogoutButton";
+
+const Dashboard = () => {
   return (
     <div>
       <h2>Welcome to the Dashboard</h2>
-      {/* Add dashboard content here */}
+      <LogoutButton />
     </div>
   );
-}
+};
 
 export default Dashboard;
